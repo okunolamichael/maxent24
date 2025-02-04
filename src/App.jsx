2a07c17173e0852b0d939d840a680aeccf4deb99
okunolamichael <michaeloolamide@gmail.com>
2025-02-04 11:50:04 -0800

    implementing router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {CssBaseline} from '@mui/material'
+import {Route, Routes} from 'react-router-dom'
+// import Home from './Home'
+// import Movies from './Movies'
+
 
 const App = () => {
   return (
     <div>
-      App
+      <CssBaseline />
+      <main>
+        <Routes>
+        <Route exact path='/' element={<h1>Movies</h1>} />
+        <Route exact path='/profile/:id' element={<h1>Profile</h1>} />
+        <Route exact path='/movie/:id' element={<h1>Movie Information</h1>} />
+        <Route exact path='/actors/:id' element={<h1>Actors</h1>} />
+        </Routes>
+      </main>
     </div>
   )
 }
