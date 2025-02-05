0b213149c204035f5ba8b0b561aae5cca81e1675
okunolamichael <michaeloolamide@gmail.com>
2025-02-05 12:36:17 -0800

    styling NavBar

--- src/main.jsx
+++ src/main.jsx
@@ -17,11 +17,16 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import {createTheme, ThemeProvider} from '@mui/material/styles'
+
+const theme = createTheme({})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme = {theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>,
   </React.StrictMode>,
 )
