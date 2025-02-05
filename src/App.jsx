7de227801f6163e749be52871e8126d2246be04a
okunolamichael <michaeloolamide@gmail.com>
2025-02-05 04:13:50 -0800

    started styling with mui

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import {CssBaseline} from '@mui/material'
 import {Route, Routes} from 'react-router-dom'
 import {Actors, MovieInformation, Movies, NavBar, Profile} from './components/index'
-
+import useStyles from './components/styles'
 
 const App = () => {
+
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
       <NavBar />
-      <main>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Routes>
         <Route exact path='/' element={<Movies />} />
         <Route exact path='/profile/:id' element={<Profile />} />
