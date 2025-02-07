936cddd893320cb81adff1bf84d6e76aabc46922
okunolamichael <michaeloolamide@gmail.com>
2025-02-07 07:44:05 -0800

    first API implementation

--- src/main.jsx
+++ src/main.jsx
@@ -18,15 +18,20 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import {createTheme, ThemeProvider} from '@mui/material/styles'
+import { Provider } from 'react-redux'
+import store from './app/store'
+
 
 const theme = createTheme({})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
     <ThemeProvider theme = {theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ThemeProvider>,
+    </ThemeProvider>
+    </Provider>,
   </React.StrictMode>,
 )
