b3fe5d4de9128e4aed5489b63e87f0fa43956fa8
okunolamichael <michaeloolamide@gmail.com>
2025-03-01 02:19:10 -0800

    syling the home page

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import useStyles from './styles';
 // import Search from './components/Search';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
@@ -14,9 +15,14 @@ const API_OPTIONS = {
 
 const Movies = () => {
   // const [searchTerm, setSearchTerm] = useState('');
+  const classes = useStyles();
   const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
     try {
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
@@ -26,10 +32,20 @@ const Movies = () => {
       }
 
       const data = await response.json();
-      console.log(data);
+      
+      if(data.Response === "False") {
+        setErrorMessage(data.Error || 'Error fetching movies');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.');
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -51,9 +67,23 @@ const Movies = () => {
           {/* <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} /> */}
         </header>
 
-        <section>
-          <h2>All Movies</h2>
-          {errorMessage && <p className="error">{errorMessage}</p>}
+        <section className={classes.allmovies}>
+          <h2 className='mt-[40px]'>All Movies</h2>
+          {isLoading ? (
+          <p className='text-white'>Loading...</p>
+        ) : errorMessage ? (
+          <p className='text-red-500'>{errorMessage}</p>
+        ) : (
+          <ul className={classes.allmoviesUl}>
+            {movieList.map((movie) => (
+              <li key={movie.id}>
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className={classes.allmovieImg}/>
+                <h3 className='text-white'>{movie.title}</h3>
+                <p>{movie.overview}</p>
+              </li>
+            ))}
+          </ul>
+        )}
         </section>
       </div>
     </main>
